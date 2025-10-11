0aad73e6604631808c89051b1fb6b0a9e999d32e
Carlos Vera <vera.0214@outlook.com>
2025-10-10 21:34:54 -0600

    assignmet 2

--- registration.js
+++ registration.js
@@ -33,3 +33,21 @@ function calculateAverageAge(){
 displayPetCount();
 displayPetNames();
 calculateAverageAge();
+
+// constructor
+
+function Pet(name, age, gender, breed, service, type) {
+    this.name = name,
+    this.age = age,
+    this.gender = gender;
+    this.breed = breed;
+    this.service = service
+    
+}
+
+let pet3 = new Pet("Rocky", 2, "Male", "Chihuahua", "Bath");
+let pet4 = new Pet("Hunter", 5, "Male", "Husky", "Hair Cut");
+let pet5 = new Pet("Brook", 3, "Male", "Boxer", "Nail");
+
+let petsCostructor = [pet3, pet4, pet5];
+console.log("Constructor Pets:", petsCostructor);
